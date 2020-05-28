0c1f2708ebcae3744cd02058f5cc2b71c0f03d24
Evren Ceyhan <webceyhan@ceymac.lan>
2020-05-28 23:51:33 +0200

    hero list / detail impl

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+
 import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
 
 @Component({
     selector: 'app-heroes',
@@ -7,12 +9,14 @@ import { Hero } from '../hero';
     styleUrls: ['./heroes.component.css'],
 })
 export class HeroesComponent implements OnInit {
-    hero: Hero = {
-        id: 1,
-        name: 'Windstorm',
-    };
+    heroes: Hero[] = HEROES;
+    selectedHero: Hero;
 
     constructor() {}
 
     ngOnInit(): void {}
+
+    onSelect(hero: Hero): void {
+        this.selectedHero = hero;
+    }
 }
