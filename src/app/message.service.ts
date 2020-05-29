2fdaa1c9c910f77d308d18b4d51ec1e2c65c0715
Evren Ceyhan <webceyhan@ceymac.lan>
2020-05-29 13:46:31 +0200

    obsrvable message service impl

--- src/app/message.service.ts
+++ src/app/message.service.ts
@@ -1,18 +1,23 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
     providedIn: 'root',
 })
 export class MessageService {
-    messages: string[] = [];
+    private messages$ = new BehaviorSubject<string[]>([]);
 
     constructor() {}
 
+    list() {
+        return this.messages$.asObservable();
+    }
+
     add(message: string) {
-        this.messages.push(message);
+        this.messages$.next([...this.messages$.value, message]);
     }
 
     clear() {
-        this.messages = [];
+        this.messages$.next([]);
     }
 }
--- src/app/messages/messages.component.ts
+++ src/app/messages/messages.component.ts
@@ -1,16 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import { MessageService } from '../message.service';
+import { Observable } from 'rxjs';
 
 @Component({
-  selector: 'app-messages',
-  templateUrl: './messages.component.html',
-  styleUrls: ['./messages.component.css']
+    selector: 'app-messages',
+    templateUrl: './messages.component.html',
+    styleUrls: ['./messages.component.css'],
 })
 export class MessagesComponent implements OnInit {
 
-  constructor(public messageSvc:MessageService) { }
+    messages$: Observable<string[]>;
 
-  ngOnInit(): void {
-  }
 
+    constructor(private messageSvc: MessageService) {}
+
+    ngOnInit(): void {
+        this.messages$ = this.messageSvc.list();
+    }
+
+    onClear() {
+        this.messageSvc.clear();
+    }
 }
